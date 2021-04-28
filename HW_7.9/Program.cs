8155d2b891405ad81d080e3cc4d1c8fab4583d15
basarYargici <ibrahimbyargici@gmail.com>
2021-04-28 06:40:11 +0300

    Add project files.

---
+++ HW_7.9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_7._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
