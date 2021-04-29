8a84077b827b1d13c36b502d085eb430421ba406
basarYargici <ibrahimbyargici@gmail.com>
2021-04-30 00:06:19 +0300

    HW_6.13 implementation started.

--- HW_6.13/Form1.Designer.cs
+++ HW_6.13/Form1.Designer.cs
@@ -29,13 +29,172 @@ namespace HW_6._13
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblHeadCount = new System.Windows.Forms.Label();
+            this.lblTailCount = new System.Windows.Forms.Label();
+            this.lblHead = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnPress = new System.Windows.Forms.Button();
+            this.lblText = new System.Windows.Forms.Label();
+            this.lblTail = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.543F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.46683F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 21.90121F));
+            this.tableLayoutPanel1.Controls.Add(this.lblHeadCount, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblTailCount, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblHead, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnPress, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblText, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblTail, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(44, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 61.10688F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.44656F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.44656F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(711, 413);
+            this.tableLayoutPanel1.TabIndex = 9;
+            // 
+            // lblHeadCount
+            // 
+            this.lblHeadCount.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblHeadCount.AutoSize = true;
+            this.lblHeadCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblHeadCount.Location = new System.Drawing.Point(557, 332);
+            this.lblHeadCount.Name = "lblHeadCount";
+            this.lblHeadCount.Size = new System.Drawing.Size(151, 81);
+            this.lblHeadCount.TabIndex = 13;
+            this.lblHeadCount.Text = "0";
+            this.lblHeadCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTailCount
+            // 
+            this.lblTailCount.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTailCount.AutoSize = true;
+            this.lblTailCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTailCount.Location = new System.Drawing.Point(557, 252);
+            this.lblTailCount.Name = "lblTailCount";
+            this.lblTailCount.Size = new System.Drawing.Size(151, 80);
+            this.lblTailCount.TabIndex = 14;
+            this.lblTailCount.Text = "0";
+            this.lblTailCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblHead
+            // 
+            this.lblHead.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblHead.AutoEllipsis = true;
+            this.lblHead.AutoSize = true;
+            this.lblHead.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblHead.Location = new System.Drawing.Point(354, 332);
+            this.lblHead.Margin = new System.Windows.Forms.Padding(16, 0, 3, 0);
+            this.lblHead.Name = "lblHead";
+            this.lblHead.Size = new System.Drawing.Size(197, 81);
+            this.lblHead.TabIndex = 12;
+            this.lblHead.Text = "Head Count";
+            this.lblHead.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.ImageLocation = "";
+            this.pictureBox.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(332, 246);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 15;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.WaitOnLoad = true;
+            // 
+            // btnPress
+            // 
+            this.btnPress.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPress.AutoEllipsis = true;
+            this.btnPress.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnPress.Location = new System.Drawing.Point(3, 335);
+            this.btnPress.Name = "btnPress";
+            this.btnPress.Size = new System.Drawing.Size(332, 75);
+            this.btnPress.TabIndex = 9;
+            this.btnPress.Text = "Toss";
+            this.btnPress.UseVisualStyleBackColor = true;
+            this.btnPress.Click += new System.EventHandler(this.BtnPressClick);
+            // 
+            // lblText
+            // 
+            this.lblText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblText.AutoSize = true;
+            this.lblText.BackColor = System.Drawing.SystemColors.Control;
+            this.lblText.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblText.Location = new System.Drawing.Point(354, 0);
+            this.lblText.Margin = new System.Windows.Forms.Padding(16, 0, 3, 0);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(197, 252);
+            this.lblText.TabIndex = 10;
+            this.lblText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblTail
+            // 
+            this.lblTail.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTail.AutoEllipsis = true;
+            this.lblTail.AutoSize = true;
+            this.lblTail.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTail.Location = new System.Drawing.Point(354, 252);
+            this.lblTail.Margin = new System.Windows.Forms.Padding(16, 0, 3, 0);
+            this.lblTail.Name = "lblTail";
+            this.lblTail.Size = new System.Drawing.Size(197, 80);
+            this.lblTail.TabIndex = 11;
+            this.lblTail.Text = "Tail Count";
+            this.lblTail.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblHeadCount;
+        private System.Windows.Forms.Label lblTailCount;
+        private System.Windows.Forms.Label lblHead;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnPress;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Label lblTail;
     }
 }
 
--- HW_6.13/Form1.cs
+++ HW_6.13/Form1.cs
@@ -12,9 +12,43 @@ namespace HW_6._13
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+        string txt = "";
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private bool Flip()
+        {
+            int number = random.Next(1, 3); // returns either 1 or 2
+            //head
+            if (number == 1) return true;
+            //tail
+            return false;
+        }
+
+        private void BtnPressClick(object sender, EventArgs e)
+        {
+
+            bool isHead = Flip();
+
+            if (isHead)
+            {
+                lblHeadCount.Text = (int.Parse(lblHeadCount.Text) + 1).ToString();
+                txt = "HEAD";
+                pictureBox.Image = new System.Drawing.Bitmap(Properties.Resources.head);
+            }
+            else
+            {
+                lblTailCount.Text = (int.Parse(lblTailCount.Text) + 1).ToString();
+                txt = "TAIL";
+                pictureBox.Image = new System.Drawing.Bitmap(Properties.Resources.tail);
+
+            }
+            lblText.Text = txt;
+        }
+
     }
 }
